34b6549ec2f22cbd3e601b429331a8d561280f61
Zhanwen Chen <phil@zhanwenchen.com>
2018-07-05 14:01:02 -0500

    (Feature) Stricter improvement standard for patience; adjust hyperparams

--- create_50_models_and_train.sh
+++ create_50_models_and_train.sh
@@ -1,1 +1,1 @@
-python lib/create_hyperparam_search.py 50 hyperparam_ranges.json && python lib/main.py DNNs/
+python lib/create_hyperparam_search.py 2 hyperparam_ranges.json && python lib/main.py DNNs/
