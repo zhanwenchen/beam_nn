cde8ab82f8432ca33eeeea0b366f26966f244e24
Zhanwen Chen <phil@zhanwenchen.com>
2018-07-17 12:48:38 -0500

    deleted commented out lines

--- lib/evaluate_models.py
+++ lib/evaluate_models.py
@@ -3,31 +3,28 @@
 #    Runs all models prefixed by an identifier on simulated cyst, phantom cyst,
 #    and in vivo data.
 # Usage:
-#    python evaluate_models.py
+#    python evaluate_models.py 123984
 import glob
 import os
 from subprocess import Popen
 import argparse
-
 
 from utils import read_model_params
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    # parser.add_argument('model_params_path', help='Option to load model params from a file. Values in this file take precedence.')
     parser.add_argument('identifier', help='Option to load model params from a file. Values in this file take precedence.')
     args = parser.parse_args()
 
     identifier = args.identifier
 
-    # last_identifier = read_model_params(os.path.join('DNNs', 'last_identifier.txt'))['last_identifier']
     models = glob.glob(os.path.join('DNNs', str(identifier)))
     num_models = len(models)
 
     for model_index, model_folder in enumerate(models):
-
-        if os.path.isfile(os.path.join(model_folder, 'scan_batteries', 'target_in_vivo', 'target_17', 'dnn.png')):
+        if os.path.isfile(os.path.join(model_folder, 'scan_batteries', 'target_in_vivo', 'target_17', 'dnn.png'))
+            and os.path.isfile(os.path.join(model_folder, 'scan_batteries', 'target_in_vivo', 'target_19', 'dnn.png')):
             continue
         commands = [
             './lib/process_single_scan_battery_anechoic_cyst.sh ' + model_folder,
@@ -40,7 +37,3 @@ if __name__ == '__main__':
         Popen(commands[1], shell=True).wait()
         print('\n\nevaluate_models.py: processing in vivo for model', model_index + 1, 'of', num_models, ':', os.path.basename(model_folder), '\n\n')
         Popen(commands[2], shell=True).wait()
-
-        # processes = [Popen(cmd, shell=True) for cmd in commands]
-        # wait for completion
-        # for p in processes: p.wait()
