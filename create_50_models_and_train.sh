a44fd983f8cab8470dc3d4db140d5374cccdee5e
Zhanwen Chen <phil@zhanwenchen.com>
2018-07-25 11:32:16 -0500

    (Refactor) Delete unuseful convenience scripts

--- create_50_models_and_train.sh
+++
@@ -1,1 +0,0 @@
-python lib/create_and_train.py 50 hyperparam_ranges.json
--- lib/create_and_train.py
+++
@@ -1,6 +0,0 @@
-from create_models import main as create
-from train import train
-
-if __name__ == '__main__':
-    identifier = create()
-    train(identifier)
