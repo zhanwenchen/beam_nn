289eaa3f0fafee4512ff474cf7393da364490b8f
Zhanwen Chen <phil@zhanwenchen.com>
2018-07-27 11:33:37 -0500

    trying to fix import error

--- lib/r3_dnn_apply.py
+++ lib/r3_dnn_apply.py
@@ -9,10 +9,10 @@ from scipy.io import savemat
 import time
 
 # modify path
-import sys
-sys.path.insert(0, '../../../../../lib')
-from lenet import LeNet
-from utils import read_model_params
+# import sys
+# sys.path.insert(0, '../../../../../lib')
+from lib.lenet import LeNet
+from lib.utils import read_model_params
 
 
 if __name__ == "__main__":
