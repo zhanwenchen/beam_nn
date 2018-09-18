7c925029f134735eabe698eafc9e9c5e8a9cd6a4
Zhanwen Chen <phil@zhanwenchen.com>
2018-09-18 10:10:44 -0500

    (Feature) Add all speckle stats in analysis df

--- notebooks/analysis_utils.py
+++ notebooks/analysis_utils.py
@@ -16,6 +16,23 @@ from lib.utils import read_model_params
 speckle_stats_cnr_idx = 1
 
 
+# CR
+def append_speckle_stats(df, model_idx, column_name, stat_name, columns_stat_das, columns_stat_dnn, speckle_stats_das, speckle_stats_dnn, index):
+    stat_das = speckle_stats_das[index]
+    stat_dnn = speckle_stats_dnn[index]
+
+    column_name_stat_das = column_name + '_' + stat_name + '_das'
+    column_name_stat_dnn = column_name + '_' + stat_name + '_dnn'
+
+    columns_stat_das.append(column_name_stat_das)
+    columns_stat_dnn.append(column_name_stat_dnn)
+
+    df.loc[model_idx, column_name_stat_das] = stat_das
+    df.loc[model_idx, column_name_stat_dnn] = stat_dnn
+
+    return columns_stat_das, columns_stat_dnn
+
+
 def get_df(identifier):
     # TODO: 1. Use scan_batteries to load das stats instead of walk through models.
     # TODO: 2. Walk model_folders only once.
@@ -56,18 +73,31 @@ def get_df(identifier):
         model_name = os.path.basename(model_folder)
         scan_batteries = glob.glob(os.path.join(model_folder, 'scan_batteries', '*'))
 
-        # print('analysis_utils: processing model', model_idx, 'of', len(model_folders), model_name)
-
         for scan_battery_folder in scan_batteries:
             target_folders = glob.glob(os.path.join(scan_battery_folder, 'target_*'))
 
             scan_battery_name = os.path.basename(scan_battery_folder)
+
+            columns_cr_das = []
+            columns_cr_dnn = []
 
             columns_cnr_das = []
             columns_cnr_dnn = []
 
             columns_snr_das = []
             columns_snr_dnn = []
+
+            columns_mean_in_das = []
+            columns_mean_in_dnn = []
+
+            columns_mean_out_das = []
+            columns_mean_out_dnn = []
+
+            columns_var_in_das = []
+            columns_var_in_dnn = []
+
+            columns_var_out_das = []
+            columns_var_out_dnn = []
 
 
             for target_idx, target_folder in enumerate(target_folders):
@@ -77,39 +107,45 @@ def get_df(identifier):
                 speckle_stats_das = pd.read_csv(speckle_stats_das_fname, delimiter=",", header=None).values
                 speckle_stats_dnn = pd.read_csv(speckle_stats_dnn_fname, delimiter=",", header=None).values
 
-                cnr_das = speckle_stats_das[1]
-                cnr_dnn = speckle_stats_dnn[1]
-
-                snr_das = speckle_stats_das[2]
-                snr_dnn = speckle_stats_dnn[2]
-
                 column_name = '_'.join([scan_battery_name, os.path.basename(target_folder)])
 
+                # CR
+                columns_cr_das, columns_cr_dnn = append_speckle_stats(df, model_idx, column_name, 'cr', columns_cr_das, columns_cr_dnn, speckle_stats_das, speckle_stats_dnn, 0)
                 # CNR
-                column_name_cnr_das = column_name + '_cnr_das'
-                column_name_cnr_dnn = column_name + '_cnr_dnn'
-
-                columns_cnr_das.append(column_name_cnr_das)
-                columns_cnr_dnn.append(column_name_cnr_dnn)
-
-                df.loc[model_idx, column_name_cnr_das] = cnr_das
-                df.loc[model_idx, column_name_cnr_dnn] = cnr_dnn
-
+                columns_cnr_das, columns_cnr_dnn = append_speckle_stats(df, model_idx, column_name, 'cnr', columns_cnr_das, columns_cnr_dnn, speckle_stats_das, speckle_stats_dnn, 1)
                 # SNR
-                column_name_snr_das = column_name + '_snr_das'
-                column_name_snr_dnn = column_name + '_snr_dnn'
+                columns_snr_das, columns_snr_dnn = append_speckle_stats(df, model_idx, column_name, 'snr', columns_snr_das, columns_snr_dnn, speckle_stats_das, speckle_stats_dnn, 2)
+                # mean_in
+                columns_mean_in_das, columns_mean_in_dnn = append_speckle_stats(df, model_idx, column_name, 'mean_in', columns_mean_in_das, columns_mean_in_dnn, speckle_stats_das, speckle_stats_dnn, 3)
+                # mean_out
+                columns_mean_out_das, columns_mean_out_dnn = append_speckle_stats(df, model_idx, column_name, 'mean_out', columns_mean_out_das, columns_mean_out_dnn, speckle_stats_das, speckle_stats_dnn, 4)
+                # var_in
+                columns_var_in_das, columns_var_in_dnn = append_speckle_stats(df, model_idx, column_name, 'var_in', columns_var_in_das, columns_var_in_dnn, speckle_stats_das, speckle_stats_dnn, 5)
+                # var_out
+                columns_var_out_das, columns_var_out_dnn = append_speckle_stats(df, model_idx, column_name, 'var_out', columns_var_out_das, columns_var_out_dnn, speckle_stats_das, speckle_stats_dnn, 6)
 
-                columns_snr_das.append(column_name_snr_das)
-                columns_snr_dnn.append(column_name_snr_dnn)
 
-                df.loc[model_idx, column_name_snr_das] = snr_das
-                df.loc[model_idx, column_name_snr_dnn] = snr_dnn
+            # Average speckle stats across targets
+            df.loc[:, scan_battery_name + '_avg_cr_das'] = df.loc[:, columns_cr_das].mean(axis=1)
+            df.loc[:, scan_battery_name + '_avg_cr_dnn'] = df.loc[:, columns_cr_dnn].mean(axis=1)
 
             df.loc[:, scan_battery_name + '_avg_cnr_das'] = df.loc[:, columns_cnr_das].mean(axis=1)
             df.loc[:, scan_battery_name + '_avg_cnr_dnn'] = df.loc[:, columns_cnr_dnn].mean(axis=1)
 
             df.loc[:, scan_battery_name + '_avg_snr_das'] = df.loc[:, columns_snr_das].mean(axis=1)
             df.loc[:, scan_battery_name + '_avg_snr_dnn'] = df.loc[:, columns_snr_dnn].mean(axis=1)
+
+            df.loc[:, scan_battery_name + '_avg_mean_in_das'] = df.loc[:, columns_mean_in_das].mean(axis=1)
+            df.loc[:, scan_battery_name + '_avg_mean_in_dnn'] = df.loc[:, columns_mean_in_dnn].mean(axis=1)
+
+            df.loc[:, scan_battery_name + '_avg_mean_out_das'] = df.loc[:, columns_mean_out_das].mean(axis=1)
+            df.loc[:, scan_battery_name + '_avg_mean_out_dnn'] = df.loc[:, columns_mean_out_dnn].mean(axis=1)
+
+            df.loc[:, scan_battery_name + '_avg_var_in_das'] = df.loc[:, columns_var_in_das].mean(axis=1)
+            df.loc[:, scan_battery_name + '_avg_var_in_dnn'] = df.loc[:, columns_var_in_dnn].mean(axis=1)
+
+            df.loc[:, scan_battery_name + '_avg_var_out_das'] = df.loc[:, columns_var_out_das].mean(axis=1)
+            df.loc[:, scan_battery_name + '_avg_var_out_dnn'] = df.loc[:, columns_var_out_dnn].mean(axis=1)
 
     # Convert model params columns to numeric
     numeric_columns = [ \
