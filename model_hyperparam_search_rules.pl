6d679f3a29a69f4a103eac75359e50b96cce0522
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2019-05-19 12:40:22 -0500

    (Feature) Successfully implemented single-parameter random partial search

--- model_hyperparam_search_rules.pl
+++ model_hyperparam_search_rules.pl
@@ -35,8 +35,23 @@ is_network_legal(InputSizes, [CurrentLayer|RestLayers]) :-
 
 %% Conv1 = conv1{type: conv2d, out_channels: 96, kernel_height: 11, kernel_width: 11, padding_height: 0, padding_width: 0, stride_height: 4, stride_width: 4}, get_output_size([227, 227, 3], X, [OutputHeight, OutputWidth, OutputDepth]).
 
-main :-
-  Conv1 = conv1{type: conv2d, out_channels: 96, kernel_height: 11, kernel_width: 11, padding_height: 0, padding_width: 0, stride_height: 4, stride_width: 4},
+% main :-
+%   Conv1 = conv1{type: conv2d, out_channels: 96, kernel_height: 11, kernel_width: 11, padding_height: 0, padding_width: 0, stride_height: 4, stride_width: 4},
+%   Pool1 = pool1{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
+%   Conv2 = conv2{type: conv2d, out_channels: 256, kernel_height: 5, kernel_width: 5, padding_height: 2, padding_width: 2, stride_height: 1, stride_width: 1},
+%   Pool2 = pool2{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
+%   Conv3 = conv3{type: conv2d, out_channels: 384, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
+%   Conv4 = conv4{type: conv2d, out_channels: 384, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
+%   Conv5 = conv5{type: conv2d, out_channels: 256, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
+%   Pool3 = pool3{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
+%   AlexNet = [Conv1, Pool1, Conv2, Pool2, Conv3, Conv4, Conv5, Pool3],
+%   is_network_legal([227, 227, 3], AlexNet).
+
+% https://stackoverflow.com/questions/36340495/prolog-generate-list-with-random-values
+main(Conv1KernelHeight) :-
+  % between(20, 200, Conv1KernelHeight),
+  (repeat, random_between(20, 200, Conv1KernelHeight)),
+  Conv1 = conv1{type: conv2d, out_channels: 96, kernel_height: Conv1KernelHeight, kernel_width: 11, padding_height: 0, padding_width: 0, stride_height: 4, stride_width: 4},
   Pool1 = pool1{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
   Conv2 = conv2{type: conv2d, out_channels: 256, kernel_height: 5, kernel_width: 5, padding_height: 2, padding_width: 2, stride_height: 1, stride_width: 1},
   Pool2 = pool2{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
@@ -47,25 +62,12 @@ main :-
   AlexNet = [Conv1, Pool1, Conv2, Pool2, Conv3, Conv4, Conv5, Pool3],
   is_network_legal([227, 227, 3], AlexNet).
 
-%% AlexNet = [conv1{type: conv2d, out_channels: 96, kernel_height: 11, kernel_width: 11, padding_height: 0, padding_width: 0, stride_height: 4, stride_width: 4},
-%%   pool1{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
-%%   conv2{type: conv2d, out_channels: 256, kernel_height: 5, kernel_width: 5, padding_height: 2, padding_width: 2, stride_height: 1, stride_width: 1},
-%%   pool2{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
-%%   conv3{type: conv2d, out_channels: 384, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
-%%   conv4{type: conv2d, out_channels: 384, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
-%%   conv5{type: conv2d, out_channels: 256, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
-%%   pool3{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
-%% ]
 
-%% X = conv1{type: conv2d, out_channels: 96, kernel_height: 11, kernel_width: 11, padding_height: 0, padding_width: 0, stride_height: 4, stride_width: 4},
-%% get_output_size([227, 227, 3], X, [OutputHeight, OutputWidth, OutputDepth]).
+% https://stackoverflow.com/a/41212369
 
-%% AlexNet = [conv1{type: conv2d, out_channels: 96, kernel_height: 11, kernel_width: 11, padding_height: 0, padding_width: 0, stride_height: 4, stride_width: 4},
-%%   pool1{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
-%%   conv2{type: conv2d, out_channels: 256, kernel_height: 5, kernel_width: 5, padding_height: 2, padding_width: 2, stride_height: 1, stride_width: 1},
-%%   pool2{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
-%%   conv3{type: conv2d, out_channels: 384, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
-%%   conv4{type: conv2d, out_channels: 384, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
-%%   conv5{type: conv2d, out_channels: 256, kernel_height: 3, kernel_width: 3, padding_height: 1, padding_width: 1, stride_height: 1, stride_width: 1},
-%%   pool2{type: maxpool2d, kernel_height: 3, kernel_width: 3, stride_height: 2, stride_width: 2},
-%% ]
+% Find 5 solutions for Conv1KernelHeight such that
+main_2(Listy) :-
+  once(findnsols(5, Conv1KernelHeight, main(Conv1KernelHeight), Listy)).
+
+
+% Random solutions: https://stackoverflow.com/a/41427112
